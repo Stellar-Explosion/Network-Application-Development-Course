af852f562fb0c072428ddb0fefc06adeeb09d26f
Stellar-Explosion <daniil.ryzhkov.it@gmail.com>
2023-10-29 12:49:36 +0300

    Site development. part 3

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 
 def index(request):
@@ -14,4 +18,12 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
-# Create your views here.
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
